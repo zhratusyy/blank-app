643b3674926996f01d3e6eda5354310ed44e2408
zhratusyy <zahraliashy@gmail.com>
2025-05-22 15:12:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG-20240825-WA0050.jpg", width=200)
