2270eba3fb1dfdfafacfef2df9f5471502bbd5fb
zhratusyy <zahraliashy@gmail.com>
2025-05-22 15:13:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("zhratusyy")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "(senblak)."
 )
 st.image("IMG-20240825-WA0050.jpg", width=200)
